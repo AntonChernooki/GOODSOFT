d536eaa88a96c0cb92efa9669c4fa245cd9a6b5a
AntonChernooki <cernookijanton@gmail.com>
2026-03-02 15:29:29 +0300

    привел приложение к трех слойной архитектуре - dao service web

---
+++ task5/task5/src/main/java/dao/UserDao.java
@@ -0,0 +1,13 @@
+package dao.impl;
+
+import model.User;
+
+import java.util.Collection;
+
+public interface UserDao {
+    Collection<User> getAllUsers();
+    User getUserByLogin(String login);
+    boolean addUser(User user);
+    boolean updateUser(String login, User user);
+    boolean deleteUser(String login);
+}
